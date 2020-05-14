0b885284bf4fd85fce1891f5e24ef9e82716a5b9
GeorgeVorob <54977627+GeorgeVorob@users.noreply.github.com>
2020-05-14 15:42:44 +0700

    Рефлексия

---
+++ Рефлексия/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+
+namespace Рефлексия
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Assembly Asm = Assembly.LoadFrom("../../bavito_server.exe");
+            Console.WriteLine("exe файл взят с https://github.com/GeorgeVorob/bavito-server");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(Asm.FullName);
+            Type[] types = Asm.GetTypes();
+            foreach (Type Types in types)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Тип данных: ");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(Types.Name);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"Методы: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                foreach (MethodInfo Methods in Types.GetMethods())
+                {
+                    Console.WriteLine($"   {Methods}\n");
+                }
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+            Console.WriteLine("Нажмите любую клавишу для закрытия");
+            Console.Read();
+        }
+    }
+}
