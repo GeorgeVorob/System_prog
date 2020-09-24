bac6bc16aef15d845c395662a8a71a04abc11582
GeorgeVorob <54977627+GeorgeVorob@users.noreply.github.com>
2020-09-24 03:31:51 -0400

    Лабраторная 0 завершение

--- Lab_0/Lab_0.c
+++ Lab_0/Lab_0.c
@@ -1,42 +1,29 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <errno.h>
-#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
 int main(int argc, char *argv[])
 {
-  pid_t pid;
-  int rv;
-  int i;
-  char *newargs[argc-1];
-  switch(pid=fork()) {
-  case -1:
-          perror("fork"); /* произошла ошибка */
-          exit(1); /*выход из родительского процесса*/
-  case 0:
-          printf("CHILD: Это процесс-потомок!\n");
-          printf("CHILD: Мой PID -- %d\n", getpid());
-          
-          i=0;
-          while(argv[i]!=NULL)
-          {
-                  printf("CHILD: Аргумент: %s \n",argv[i]);
-                  i++;
-          }
-          for(i=2;i<=argc-1;i++)
-          {
-                  newargs[i]=argv[i];
-          }
-          execv(argv[1],argv);
-
-  default: 
-          printf("PARENT: Это процесс-родитель!\n");
-          printf("PARENT: Мой PID -- %d\n", getpid());
-          printf("PARENT: PID моего потомка %d\n",pid);
-          printf("PARENT: Жду, пока потомок не вызовет exit()...\n");
-          printf("PARENT: Код возврата потомка:%d\n", WEXITSTATUS(rv));
-          printf("PARENT: Выход!\n");
-          exit(0);
-  }
-  return 100;
+        int childExitStatus;
+        switch (fork())
+        {
+        case -1:
+                perror("Ошибка создания нового процесса: "); /* произошла ошибка */
+                return 1;
+        case 0:
+        printf("Дочерний процесс: \n");
+        if (execvp(argv[1], &argv[2]) == -1)
+        {
+                perror("Ошибка запуска программы в дочернем процессе: ");
+                return 1;
+        }
+        break;
+        default:
+        printf("Родительский процесс: \n");
+        wait(&childExitStatus);
+        printf("Код завершения потомка: %d \n",childExitStatus);
+                break;
+        }
+        return 0;
 }
